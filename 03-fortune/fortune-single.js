b5dac5f02426e71d078fc4ce70e1eee40dfd3fbf
romadercolla <marcorode@hotmail.it>
2024-07-07 19:10:05 +0200

    ✨ chore: simple version of fortune

---
+++ 03-fortune/fortune-single.js
@@ -0,0 +1,3 @@
+const quote = "Any app that can be written in JavaScript, will eventually be written in JavaScript.\nJeff Atwood";
+
+console.log(quote);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # samplenodejs
 Sample from Node.js book by Alessandro M.
+
+## 03-fortune
+Simple clone of UNIX command *fortune*
