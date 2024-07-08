238cab47eba7100bd4e2709e98f472ac41b0f1f7
romadercolla <marcorode@hotmail.it>
2024-07-08 23:06:29 +0200

    📤 Read file from disk ⏫ update readme

---
+++ 03-fortune/05-fortune-file.js
@@ -0,0 +1,10 @@
+// Modulo per gestione del file system
+const fs = require("fs");
+
+fs.readFile("./data/003.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.log("Error while reading quote file");
+        return;
+    }
+    console.log(data);
+});
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Sample from Node.js book by Alessandro M.
 
 ## 03-fortune
 Simple clone of UNIX command *fortune*
+- 01-fortune-single.js: mixed sample
+- 05-fortune-file.js: read one file (in this sample data/003.txt) and print content
