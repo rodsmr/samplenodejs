14f78b207ca5072b0a94d4bc71feecb31a403ebd
romadercolla <marcorode@hotmail.it>
2024-07-08 22:43:40 +0200

    💡 Use json file to store quotes

--- 03-fortune/fortune-single.js
+++ 03-fortune/fortune-single.js
@@ -1,17 +1,5 @@
-const quotes = [
-`Any app that can be written in JavaScript, 
-will eventually be written in JavaScript.
-                            -- Jeff Atwood
-`,
-`JavaScript is the only language that I'm aware of
-that people feel they don't need to learn before
-they start using it.
-                            -- Douglas Crockford
-`,
-`Code never lies, comments sometimes do.
-                            -- Anonymous
-`
-];
+const quotes = require("./quotes.json");
+
 const randomIndex = Math.floor(Math.random() * quotes.length);
 
 console.log(quotes[randomIndex]);
