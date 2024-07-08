57f987c474bd83d8a0e82a3217f0226d021c1c56
romadercolla <marcorode@hotmail.it>
2024-07-08 22:36:08 +0200

    🔧 Change quote to quotes. Use random number to fortune

--- 03-fortune/fortune-single.js
+++ 03-fortune/fortune-single.js
@@ -1,7 +1,17 @@
-const quote = `
-    Any app that can be written in JavaScript, 
-    will eventually be written in JavaScript.
-                                -- Jeff Atwood
-`;
+const quotes = [
+`Any app that can be written in JavaScript, 
+will eventually be written in JavaScript.
+                            -- Jeff Atwood
+`,
+`JavaScript is the only language that I'm aware of
+that people feel they don't need to learn before
+they start using it.
+                            -- Douglas Crockford
+`,
+`Code never lies, comments sometimes do.
+                            -- Anonymous
+`
+];
+const randomIndex = Math.floor(Math.random() * quotes.length);
 
-console.log(quote);
+console.log(quotes[randomIndex]);
